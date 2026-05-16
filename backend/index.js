b60fb9b5bc68f8ca8712b40a0dc684541b8de7be
zabaoit <nguyengiabao8407@gmail.com>
2026-05-16 21:05:32 +0700

    feat: add database connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from "cors"
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+dotenv.config({});
 
 const app = express();
 
@@ -14,8 +17,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+    connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
